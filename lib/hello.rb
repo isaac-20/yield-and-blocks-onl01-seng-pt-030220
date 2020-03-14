7c8775f859ef339feba9e6b5eee8143c22f90e96
Isaac Smith <isaacsmith215@aol.com>
2020-03-14 01:14:57 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,11 +1,11 @@
 def hello_t(array)
   i = 0
   while i < array.length
-  yield array[i]
+    yield array[i]
     i += 1
   end
-end# call your method here!
-hello_t(["Tim", "Tom", "Jim"])do|name|
+end
+hello_t(["Tim", "Tom", "Jim"]).each.do|name|
       if name.start_with?("T")
         puts "Hi, #{name}"
       end
